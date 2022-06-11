372cd92fffb09df6decb2e5688674630e530947c
unknown <Nmilekic>
2022-06-11 18:01:34 +0200

    Change name of database from data1 to data.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 else:
-    uri = 'sqlite:///data1.db'
+    uri = 'sqlite:///data.db'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
